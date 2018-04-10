f44bc3f1d0679faedc8ee5e5f985a96d52a106e4
Lindsey Trainor <ltrainor38@gmail.com>
2018-04-10 03:19:45 +0000

    automatically backed up by learn

--- db/migrate/20180410031524_create_artists.rb
+++ db/migrate/20180410031524_create_artists.rb
@@ -1,4 +1,12 @@
 class CreateArtists < ActiveRecord::Migration[5.1]
-  def change
+  def up 
+    create_table :artists do |t| 
+      t.string :name 
+    end 
   end
+
+  def down 
+    drop_table :artists 
+  end 
+  
 end
