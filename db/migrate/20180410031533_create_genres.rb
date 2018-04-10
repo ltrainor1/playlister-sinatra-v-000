2cbaa9e2ea4356595898e6401cc10ce07d6ec6fc
Lindsey Trainor <ltrainor38@gmail.com>
2018-04-10 03:21:11 +0000

    automatically backed up by learn

--- db/migrate/20180410031533_create_genres.rb
+++ db/migrate/20180410031533_create_genres.rb
@@ -1,11 +1,11 @@
 class CreateGenres < ActiveRecord::Migration[5.1]
-  def up 
-    create_table :genres do |t| 
-      t.string :name 
+  def up
+    create_table :genres do |t|
+      t.string :name
   end
 
-  def down 
-    drop_table :genres 
-  end 
-  
+  def down
+    drop_table :genres
+  end
+
 end
--- db/migrate/20180410031553_create_song_genres.rb
+++ db/migrate/20180410031553_create_song_genres.rb
@@ -1,4 +1,12 @@
 class CreateSongGenres < ActiveRecord::Migration[5.1]
-  def change
+  def up 
+    create_table :song_genres do |t| 
+      t.integer :song_id
+      t.integer :genre_id 
   end
+
+  def down 
+    drop_table :artists 
+  end 
+  
 end
